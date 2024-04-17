65738cc8676884f1b8c536d8aa7174343e782f6d
Tomislav Dukez <tomdu3@ymail.com>
2024-04-17 20:22:47 +0100

    Add scipy example

---
+++ scipy_ex.py
@@ -0,0 +1,19 @@
+# scipy has a collection of mathematical algorithms and scientific functions built on the numpy  extension
+# high level commands and classes for manipulating and visualizing data
+#  multi dimensional image processing
+# includes functions for computing integrals numerically solving differential equations, optimization, etc.
+
+# application of scipy:
+# 1. multi-dimensional image processing
+# 2. solve differential equations and fourier transforms
+# 3. optimization algorithms
+# 4. linear algebra
+
+from scipy import constants
+
+
+print(constants.c)  # speed of light
+print(constants.pi)  # pi
+print(constants.e)  # e
+print(constants.h)  # planck's constant
+print(constants.N_A)  # Avogadro's number
