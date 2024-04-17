0e79c0aeb8c4d746a6b4f24c775ed45ae4054139
Tomislav Dukez <tomdu3@ymail.com>
2024-04-17 20:24:38 +0100

    Add pandas example

---
+++ pandas_ex.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(np.random.randn(5, 4), index=['a', 'c', 'e', 'f', 'h'], columns=['one', 'two', 'three', 'four'])
+
+print(df)
+print(df.loc['c'])
