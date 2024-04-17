fdb78e18ea459b92e75db3b3141f0f0504e0d493
Tomislav Dukez <tomdu3@ymail.com>
2024-04-17 18:29:43 +0100

    Add finished smtp

--- auto_smtp.py
+++ auto_smtp.py
@@ -4,4 +4,22 @@ smtp_obj = smtplib.SMTP("smtp.gmail.com", 587)  # to connect to smtp server
 smtp_obj.ehlo()  # to check the connection
 smtp_obj.starttls()  # to secure the connection
 
-smtp_obj.login(login, password)  # login to smtp server
+smtp_obj.login(login_id, password)  # login to smtp server
+
+from_addr = 'my_email@mail.com'
+to_addr = 'destination@mail.com'
+msg = '''Subject: Hello world
+I greet you humbly.
+
+Sincerely,
+Tom
+'''
+
+smtp = smtplib.SMTP('smtp.gmail.com', 587)
+smtp.ehlo()
+smtp.starttls()
+smtp.login(login_id, password)
+
+smtp.sendmail(from_addr, to_addr, msg)
+
+smtp.quit()
